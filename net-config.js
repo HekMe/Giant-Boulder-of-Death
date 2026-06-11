2a91aa14a803ee0b59fba36379c016dc9f640548
Hek <hektor157@gmail.com>
2026-06-12 00:08:51 +0200

    Create net-config.js

---
+++ net-config.js
@@ -0,0 +1,6 @@
+window.__NET__ = {
+  host: "https://maincloud.spacetimedb.com",
+  db: "rockfall-3s9js",
+  authUrl: "https://auth.spacetimedb.com/oidc",
+  clientId: "690145790734-tq1926gd8tp36hkp3kkku3udact15cjt.apps.googleusercontent.com"
+};
